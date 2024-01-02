1de772fc975a459801e02185f585098878dce6cb
roc102 <kyroc102@gmail.com>
2024-01-02 23:27:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+// import { useGoogleLogin } from '@react-oauth/google';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          const credentialResponseCode = jwtDecode(credentialResponse.credential);
+          console.log(credentialResponseCode);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </div>
   );
 }
