f6691afb04ba7c1a161b42c0bb7b2c878281fc75
roc102 <kyroc102@gmail.com>
2024-01-27 21:15:55 +0530

    opencagemap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 // import { useGoogleLogin } from '@react-oauth/google';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <GoogleLogin
-        onSuccess={credentialResponse => {
-          const credentialResponseCode = jwtDecode(credentialResponse.credential);
+        onSuccess={(credentialResponse) => {
+          console.log("credentials", credentialResponse);
+          const credentialResponseCode = jwtDecode(
+            credentialResponse.credential
+          );
           console.log(credentialResponseCode);
         }}
         onError={() => {
-          console.log('Login Failed');
+          console.log("Login Failed");
         }}
       />
     </div>
@@ -21,3 +24,42 @@ function App() {
 }
 
 export default App;
+
+// ---------- Custom Google Auth ------------
+
+// const googleLogin = useGoogleLogin({
+//     onSuccess: async (tokenResponse) => {
+//       console.log(tokenResponse);
+
+//       const userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+//       const userInfoResponse = await fetch(userInfoEndpoint, {
+//         headers: {
+//           Authorization: `Bearer ${tokenResponse.access_token}`,
+//         },
+//       });
+
+//       if (userInfoResponse.ok) {
+//         const userInfo = await userInfoResponse.json();
+//         console.log(userInfo)
+//       } else {
+//         console.error("Failed to fetch user information");
+//       }
+//     },
+//   });
+
+// <button
+//   onClick={() => {
+//     googleLogin();
+//     closeModal();
+//   }}
+//   className="border border-gray-300 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-full w-full mb-4 flex items-center justify-center"
+// >
+//   <span className="mr-2">
+//     <img
+//       src="https://in.bmscdn.com/webin/common/icons/googlelogo.svg"
+//       alt="Google Logo"
+//       className="w-6 h-6"
+//     />
+//   </span>
+//   Continue with Google
+// </button>;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="<Your Key here>">
+    <GoogleOAuthProvider clientId="957392662801-otrfg2mldvbokb4j0og09lrrfn8fahh5.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
